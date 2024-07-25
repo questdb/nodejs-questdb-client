92fd173e116578af2790c14f2ed5ad94c6a26002
javier ramírez <javier.ramirez.gomara@gmail.com>
2024-07-25 18:02:25 +0200

    [docs] Changed code examples and bumped dev dependency (#35)
    * changed code examples
    
    * changing examples to use at with a date. Bumped dependency for audit
    
    * Update README.md
    
    consistent quotes
    
    Co-authored-by: Andrei Pechkurov <37772591+puzpuzpuz@users.noreply.github.com>
    
    * Update README.md
    
    consistent quotes
    
    Co-authored-by: Andrei Pechkurov <37772591+puzpuzpuz@users.noreply.github.com>
    
    * Update README.md
    
    consistent quotes
    
    Co-authored-by: Andrei Pechkurov <37772591+puzpuzpuz@users.noreply.github.com>
    
    * Update README.md
    
    consistent quotes
    
    Co-authored-by: Andrei Pechkurov <37772591+puzpuzpuz@users.noreply.github.com>
    
    * Update README.md
    
    consistent quotes
    
    Co-authored-by: Andrei Pechkurov <37772591+puzpuzpuz@users.noreply.github.com>
    
    * Update README.md
    
    consistent quotes
    
    Co-authored-by: Andrei Pechkurov <37772591+puzpuzpuz@users.noreply.github.com>
    
    * Update README.md
    
    consistent quotes
    
    Co-authored-by: Andrei Pechkurov <37772591+puzpuzpuz@users.noreply.github.com>
    
    * Update README.md
    
    consistent quotes
    
    Co-authored-by: Andrei Pechkurov <37772591+puzpuzpuz@users.noreply.github.com>
    
    * Update examples/workers.js
    
    consistent quotes
    
    Co-authored-by: Andrei Pechkurov <37772591+puzpuzpuz@users.noreply.github.com>
    
    * Update examples/workers.js
    
    consistent quotes
    
    Co-authored-by: Andrei Pechkurov <37772591+puzpuzpuz@users.noreply.github.com>
    
    * Update README.md
    
    consistent quotes
    
    Co-authored-by: Andrei Pechkurov <37772591+puzpuzpuz@users.noreply.github.com>
    
    * Update README.md
    
    consistent quotes
    
    Co-authored-by: Andrei Pechkurov <37772591+puzpuzpuz@users.noreply.github.com>
    
    * Update README.md
    
    consistent quotes
    
    Co-authored-by: Andrei Pechkurov <37772591+puzpuzpuz@users.noreply.github.com>
    
    * Update README.md
    
    consistent quotes
    
    Co-authored-by: Andrei Pechkurov <37772591+puzpuzpuz@users.noreply.github.com>
    
    * Update README.md
    
    consistent quotes
    
    Co-authored-by: Andrei Pechkurov <37772591+puzpuzpuz@users.noreply.github.com>
    
    * Update README.md
    
    consistent quotes
    
    Co-authored-by: Andrei Pechkurov <37772591+puzpuzpuz@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Andrei Pechkurov <37772591+puzpuzpuz@users.noreply.github.com>

--- examples/auth.js
+++ examples/auth.js
@@ -2,40 +2,40 @@ const { Sender } = require('@questdb/nodejs-client');
 
 async function run() {
   // authentication details
-  const CLIENT_ID = 'testapp';
-  const PRIVATE_KEY = '9b9x5WhJywDEuo1KGQWSPNxtX-6X6R2BRCKhYMMY6n8';
+  const CLIENT_ID = 'admin'
+  const PRIVATE_KEY = 'ZRxmCOQBpZoj2fZ-lEtqzVDkCre_ouF3ePpaQNDwoQk'
   const AUTH = {
     keyId: CLIENT_ID,
     token: PRIVATE_KEY
-  };
+  }
 
   // pass the authentication details to the sender
-  const sender = new Sender({ protocol: 'tcp', host: 'localhost', port: 9009, bufferSize: 4096, auth: AUTH });
-  await sender.connect();
+  const sender = new Sender({ protocol: 'tcp', host: '127.0.0.1', port: 9009, bufferSize: 4096, auth: AUTH })
+  await sender.connect()
 
   // send the data over the authenticated connection
-  let bday = Date.parse('1856-07-10');
   await sender
-    .table('inventors_nodejs')
-    .symbol('born', 'Austrian Empire')
-    .timestampColumn('birthday', bday, 'ms') // epoch in millis
-    .intColumn('id', 0)
-    .stringColumn('name', 'Nicola Tesla')
-    .at(Date.now(), 'ms'); // epoch in millis
-  bday = Date.parse('1847-02-11');
+    .table("trades")
+    .symbol("symbol", "ETH-USD")
+    .symbol("side", "sell")
+    .floatColumn("price", 2615.54)
+    .floatColumn("amount", 0.00044)
+    .at(Date.now(), 'ms')
+
+  // add rows to the buffer of the sender
   await sender
-    .table('inventors_nodejs')
-    .symbol('born', 'USA')
-    .timestampColumn('birthday', bday, 'ms')
-    .intColumn('id', 1)
-    .stringColumn('name', 'Thomas Alva Edison')
-    .at(Date.now(), 'ms');
+    .table("trades")
+    .symbol("symbol", "BTC-USD")
+    .symbol("side", "sell")
+    .floatColumn("price", 39269.98)
+    .floatColumn("amount", 0.001)
+    .at(Date.now(), 'ms')
 
   // flush the buffer of the sender, sending the data to QuestDB
-  await sender.flush();
+  await sender.flush()
 
   // close the connection after all rows ingested
-  await sender.close();
+  await sender.close()
 }
 
-run().catch(console.error);
+run().catch(console.error)
--- examples/basic.js
+++ examples/basic.js
@@ -1,34 +1,27 @@
-const { Sender } = require('@questdb/nodejs-client');
+const { Sender } = require('@questdb/nodejs-client')
 
 async function run() {
   // create a sender with a 4KB buffer
-  const sender = new Sender({ protocol: 'tcp', host: 'localhost', port: 9009, bufferSize: 4096 });
-  await sender.connect();
+  const sender = new Sender({ protocol: 'tcp', host: '127.0.0.1', port: 9009, bufferSize: 4096 })
+  await sender.connect()
+
 
   // add rows to the buffer of the sender
-  let bday = Date.parse('1856-07-10');
   await sender
-    .table('inventors_nodejs')
-    .symbol('born', 'Austrian Empire')
-    .timestampColumn('birthday', bday, 'ms') // epoch in millis
-    .intColumn('id', 0)
-    .stringColumn('name', 'Nicola Tesla')
-    .at(Date.now(), 'ms'); // epoch in millis
-  bday = Date.parse('1847-02-11');
-  await sender
-    .table('inventors_nodejs')
-    .symbol('born', 'USA')
-    .timestampColumn('birthday', bday, 'ms')
-    .intColumn('id', 1)
-    .stringColumn('name', 'Thomas Alva Edison')
-    .at(Date.now(), 'ms');
+    .table("trades")
+    .symbol("symbol", "ETH-USD")
+    .symbol("side", "sell")
+    .floatColumn("price", 2615.54)
+    .floatColumn("amount", 0.00044)
+    .at(Date.now(), 'ms')
 
   // flush the buffer of the sender, sending the data to QuestDB
-  // the buffer is cleared after the data is sent and the sender is ready to accept new data
-  await sender.flush();
+  // the buffer is cleared after the data is sent, and the sender is ready to accept new data
+  await sender.flush()
 
-  // close the connection after all rows were sent
-  await sender.close();
+  // close the connection after all rows ingested
+  // unflushed data will be lost
+  await sender.close()
 }
 
-run().catch(console.error);
+run().then(console.log).catch(console.error)
