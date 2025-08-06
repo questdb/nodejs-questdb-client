97066b46d49ed0d68a55e00126769e616b2fda90
glasstiger <imre@questdb.io>
2025-08-06 14:14:37 +0100

    clear timeout if fetch finished

--- src/utils.ts
+++ src/utils.ts
@@ -54,7 +54,7 @@ async function fetchJson<T>(
 ): Promise<T> {
   const controller = new AbortController();
   const { signal } = controller;
-  setTimeout(() => controller.abort(), timeout);
+  const timeoutId = setTimeout(() => controller.abort(), timeout);
 
   let response: globalThis.Response;
   try {
@@ -64,6 +64,8 @@ async function fetchJson<T>(
     });
   } catch (error) {
     throw new Error(`Failed to load ${url} [error=${error}]`);
+  } finally {
+    clearTimeout(timeoutId);
   }
 
   if (!response.ok) {
